23f5e036bb5c2f2f3e342dfefb549dad965259ae
s-xiaoxiao <460046653@qq.com>
2020-01-09 22:44:23 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import x from "./x.js";
-import "./s.css";
+import png from "./assets/1.png";
 
-console.log(x);
-console.log("hi2");
+const div = document.getElementById("app");
+
+div.innerHTML = `<img src="${png}"/>`;
+
+const button = document.createElement("button");
+document.body.append(button);
+button.innerText = "懒加载";
+
+button.onclick = () => {
+  const promise = import("./lazy");
+
+  promise.then(
+    module => {
+      module.default();
+    },
+    () => {}
+  );
+};
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,32 @@ module.exports = {
       title: "xiaoxiao App",
       template: "./src/assets/index.html"
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: { implementation: require("dart-sass") }
+          }
+        ]
+      }
+    ]
+  }
 };
